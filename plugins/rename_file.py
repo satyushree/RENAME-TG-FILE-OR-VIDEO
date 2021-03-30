0fcf9ab1cebf3fe438d9c3388e0547b8bf1bfd5c
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-31 02:46:53 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -38,7 +38,7 @@ from database.database import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
-"""@pyrogram.Client.on_message(pyrogram.filters.command(["rename"]))
+@pyrogram.Client.on_message(pyrogram.filters.command(["rename"]))
 async def rename_doc(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D")
@@ -53,29 +53,8 @@ async def rename_doc(bot, update):
                     num=len(file_name)
                 )
             )
-            return"""
+            return
           
-@pyrogram.Client.on_message(pyrogram.filters.command(["rename"]))
-async def rename_doc(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
-        return
-    TRChatBase(update.from_user.id, update.text, "rename")
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Join My Updates Channel to use ME 😎 🤭**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
-            )
             return
         except Exception:
             await update.reply_text("Something Wrong. Contact my Support Group")
