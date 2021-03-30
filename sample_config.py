5062d2da87983aaf66881c141cb5a0e337012bcd
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-03-30 23:47:14 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,7 +7,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", af327dd857e0e65f80fefcf6d0af4afd)
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "1309280")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
